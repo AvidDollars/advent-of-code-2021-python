62b994091beccde8aecd6928936ea7798c8a08f0
Dimitrij Kolničenko <dimitij.kol@protonmail.com>
2022-01-10 22:17:49 +0100

    implemented 2nd challenge for the 2nd day

--- day_2-dive.py
+++ day_2-dive.py
@@ -1,11 +1,11 @@
 data_file = "files/day-1.txt"
 
-coordinate = {"x": 0, "y": 0}
+coordinate = {"x": 0, "y": 0, "aim": 0}
 
 action = {
     "forward": ("x", 1),
-    "down": ("y", 1),
-    "up": ("y", -1),
+    "down": ("aim", 1),
+    "up": ("aim", -1),
 }
 
 
@@ -20,5 +20,7 @@ with open("files/day-2.txt") as data:
         coord, multiply_by = action[direction]
         coordinate[coord] += multiply_by * number
 
+        if direction == "forward":
+            coordinate["y"] += coordinate["aim"] * number
 
 print(coordinate.get("x") * coordinate.get("y"))
