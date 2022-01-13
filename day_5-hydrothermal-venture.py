ca245817bcc8e52394892ae066f1c17fc323d532
Dimitrij Kolničenko <dimitij.kol@protonmail.com>
2022-01-13 20:13:25 +0100

    rewritten file so that it is usable for part two of the challenge and part two challenge implemented

--- day_5-hydrothermal-venture.py
+++ day_5-hydrothermal-venture.py
@@ -3,7 +3,6 @@ from collections import namedtuple, Counter
 
 data_file = "files/day-5.txt"
 numbers_re = re.compile("(\d+),(\d+)\s->\s(\d+),(\d+)")
-counter = Counter()
 
 
 def extract_numbers(row: str) -> namedtuple:
@@ -12,31 +11,49 @@ def extract_numbers(row: str) -> namedtuple:
     return coords(*(int(num) for num in numbers))
 
 
-def update_counter(counter: Counter, *, x1: int, x2: int, y1: int, y2: int) -> None:
-    if (x1 > x2):
-        x1, x2 = x2, x1
+def update_counter(counter: Counter, *, x1: int, x2: int, y1: int, y2: int, straight_line: bool) -> None:
+    if straight_line:
+        if (x1 > x2):
+            x1, x2 = x2, x1
 
-    if (y1 > y2):
-        y1, y2 = y2, y1
+        if (y1 > y2):
+            y1, y2 = y2, y1
 
-    for x in range(x1, x2 + 1):
-        for y in range(y1, y2 + 1):
-            counter[(x, y)] += 1
+        for x in range(x1, x2 + 1):
+            for y in range(y1, y2 + 1):
+                counter[(x, y)] += 1
+
+    elif not straight_line:
+        if x1 > x2:
+            x1, x2 = x2, x1
+            y1, y2 = y2, y1
+
+        y_to_add = -1 if y1 > y2 else 1
+
+        while x2 >= x1:
+            counter[(x1, y1)] += 1
+            x1 += 1
+            y1 += y_to_add
 
 
-def count_overlaps(counter: Counter) -> int:
-    overlaps = 0
-    for coords, counts in counter.items():
-        if counts > 1:
-            overlaps += 1
-    return overlaps
+def count_overlaps(*, only_straight_lines: bool = True) -> int:
+    counter = Counter()
+
+    with open(data_file) as data:
+        for row in data:
+            x1, y1, x2, y2 = extract_numbers(row)
+
+            if x1 == x2 or y1 == y2:
+                update_counter(counter, x1=x1, x2=x2, y1=y1, y2=y2, straight_line=True)
+
+            elif only_straight_lines is False:
+                update_counter(counter, x1=x1, x2=x2, y1=y1, y2=y2, straight_line=False)
+
+    return sum(1 for counts in counter.values() if counts > 1)
 
 
-with open(data_file) as data:
-    for row in data:
-        x1, y1, x2, y2 = extract_numbers(row)
+# PART ONE
+print(count_overlaps(only_straight_lines=True))
 
-        if x1 == x2 or y1 == y2:
-            update_counter(counter, x1=x1, x2=x2, y1=y1, y2=y2)
-
-print(count_overlaps(counter))
+# PART TWO
+print(count_overlaps(only_straight_lines=False))
